8559b3df00819cb8b07d2c301df0a9626e651f26
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-07-01 10:28:08 +0530

    js

---
+++ pattern.js
@@ -0,0 +1,18 @@
+//  Qst 1
+
+// let num =5;
+// let star ="";
+// for(let i=1; i<=num; i++){
+//     for(let j=1; j<=num; j++){
+//         if(i==j){
+//             star += "*";
+//         } else if(i+j == 6){
+//             star += "*";
+//         } else{
+//             star += " ";
+//         }
+//     }
+//     star += "\n";
+// }
+
+// console.log(star);
