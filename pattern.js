16e6e7c97bbaddf7804571400f10a9d5d8c20311
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-07-01 18:06:39 +0530

    js

--- pattern.js
+++ pattern.js
@@ -20,13 +20,44 @@
 
 // qst 2
 
-let n=5;
-for(let i =1; i<=n; i++){
-    for(let j=1; j<=n; j++){
-        
-    }
-}
-
+// let n = 3;
+// let string = "";
+// // Upside pyramid
+// // upside diamond
+// for (let i = 1; i <= n; i++) {
+//   // printing spaces
+//   for (let j = n; j > i; j--) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < i * 2 - 1; k++) {
+//     if (k === 0 || k === 2 * i - 2) {
+//       string += "*";
+//     }
+//     else {
+//       string += " ";
+//     }
+//   }
+//   string += "\n";
+// }
+// // downside diamond
+// for (let i = 1; i <= n - 1; i++) {
+//     // printing spaces
+//     for (let j = 0; j < i; j++) {
+//       string += " ";
+//     }
+//     // printing star
+//     for (let k = (n - i) * 2 - 1; k >= 1; k--) {
+//       if (k === 1 || k === (n - i) * 2 - 1) {
+//         string += "*";
+//       }
+//       else {
+//         string += " ";
+//       }
+//     }
+//     string += "\n";
+//   }
+// console.log(string);
 
 
 // Qst 3
@@ -62,3 +93,14 @@ for(let i =1; i<=n; i++){
 
 //Qst 5
 
+let n=5;
+let count =0;
+let star = "";
+for(let i=0; i<n; i++) {
+    for(let j=0; j<n; j++) {
+        star = star + `${count}`;
+        count++;
+    }
+    star += "\n";
+}
+console.log(star)
